dd35755082be8f9bdb0d588ef07c09eac0f3a92e
John McDonnell <john.v.mcdonnell@gmail.com>
2013-03-15 11:49:12 -0400

    Fixed bug in calculating the t-distribution, this is very important for anyone modeling a continuous distribution

--- Types.hs
+++ Types.hs
@@ -56,29 +56,22 @@ type PDFFromSample = V.Vector Double -> (Maybe Double -> Double, Double)
 fillerDistribtution :: PDFFromSample
 fillerDistribtution sample = ((\x -> 0.5), 0.5)
 
-tatval :: Double -- ^ df
-       -> Double -- ^ x
-       -> Double -- ^ PDF evaluated at x
-tatval = StatDist.density . studentT
-
 -- | t posterior, used for continuous-valued dimensions
 tPosterior :: (Double, Double, Double, Double) -> PDFFromSample
-tPosterior prior sample  = (pdfFun, mui)
+tPosterior (mu0, sigma0, alpha0, lambda0) sample  = (pdfFun, mui)
   where
-    pdfFun (Just query) = tatval alphai ((query - mui) / tstdev)
+    pdfFun (Just query) = StatDist.density (studentTGeneral alphai mui tsigma) query
     pdfFun Nothing = 1
-    tstdev = sigmai * (sqrt (1 + (1/lambdai)))
+    tsigma = sigmai * (sqrt (1 + (1/lambdai)))
     sigmai = sqrt $ (alpha0*(sq sigma0) + 
-                      (n-1)*variance +
-                      (lambda0*n/(lambda0+n))*(sq (mu0-xbar))
-                    ) / (alpha0 + n)
-    mui = (lambda0*mu0 + n * xbar) / (lambda0 + n)
-    variance = if n>0 then varianceUnbiased sample else 0 -- TODO: is varianceUnbiased correct here? Also is this the right way to deal with a small sample?
-    xbar = if n>0 then mean sample else mu0
+                      (n-1)*varianceunb +
+                      (lambda0*n/lambdai)*(sq (mu0-xbar))
+                    ) / alphai
+    mui = (lambda0*mu0 + n * xbar) / lambdai
+    (xbar, varianceunb) = meanVarianceUnb sample
     lambdai = lambda0 + n
     alphai = alpha0 + n
     n = fromIntegral $ V.length sample
-    (mu0, sigma0, alpha0, lambda0) = prior
 
 -- | Bernoulli posterior, used for discrete-valued dimensions
 bernoulliPosterior :: [Double] -> PDFFromSample
@@ -106,3 +99,5 @@ dirichletProcess alpha assignments = pPartitions ++ [pNew]
     pPartitions = map (\x -> (fromIntegral x) / (n+alpha)) counts
     counts = map length $ (group . sort) assignments
     n = fromIntegral $ length assignments
+
+
