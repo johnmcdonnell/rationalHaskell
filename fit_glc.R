1365abe534ef0ec69a78dd6ffd3620aaf5ecbde4
John McDonnell <john.v.mcdonnell@gmail.com>
2013-03-24 00:45:31 -0400

    Updates to analysis file, including a bug where bias wasn't entered.

--- fit_glc.R
+++ fit_glc.R
@@ -218,7 +218,7 @@ run_anderson_once <- function(alpha, nlab, order, encoding, bias=0, sigma0=0, a0
 # }}}1
 
 # {{{1 Simulation code
-simulate_anderson <- function(alpha=1, nlab=16, order="interspersed", encoding="encodeactual", bias=0, tau=.05, plotting=F, echo=F, ...) {
+simulate_anderson <- function(alpha=1, nlab=16, order="interspersed", encoding="encodeactual", tau=.05, plotting=F, echo=F, ...) {
     #args <- list(...)
     #print(args$sigma0)
     resps <- run_anderson_once(alpha, nlab, order, encoding, ...)
@@ -246,7 +246,7 @@ simulate_anderson <- function(alpha=1, nlab=16, order="interspersed", encoding="
 }
 
 plot_anderson <- function(alpha=1, nlab=16, order="interspersed", encoding="encodeactual", ...) {
-    resps <- run_anderson_once(...)
+    resps <- run_anderson_once(alpha, nlab, order, encoding, ...)
     all.stims <- subset(resps, type=="STIM")
     stims <- subset(all.stims, bimod>0 & unimod>0 ) # Remove invisible
     # BUG somtimes there is no cluster assigned number 0.
@@ -291,12 +291,6 @@ test.clust.proportion <- function() {
 }
 
 
-#plot_anderson(cparam=1, order="interspersed")
-#plot_anderson(cparam=.7/.3, order="interspersed")
-#plot_anderson(cparam=.7/.3, nlab=4, order="interspersed")
-#plot_anderson(cparam=.7/.3, order="lablast" )
-#plot_anderson(cparam=.7/.3, order="labfirst" )
-
 run_sims <- function(runs, nreps, ofile) {
     test_param_set <- function(df) {
         count <- 0
@@ -323,7 +317,8 @@ run_sims <- function(runs, nreps, ofile) {
 
 
 # {{{2 Try one run
-plot_anderson(alpha=1, nlab=16, bias=2)
+simulate_anderson(alpha=2.333, nlab=-1, bias=100, a0=10, lambda0=1, sigma0=0.15, tau=0.05, plotting=T, echo=F)
+plot_anderson(alpha=1, lambda0=1, a0=10, nlab=16, bias=2)
 # It looks like negative bias is weaker than positive.
 #b <- rnorm(1, mean=0, sd=4)
 simulate_anderson(alpha=2.333, nlab=-1, bias=7, sigma0=0.35, tau=0.05, plotting=T, echo=T)
@@ -333,10 +328,10 @@ simulate_anderson(alpha=2.333, nlab=16, bias=0, plotting=T)
 simulate_anderson(alpha=1, nlab=16, bias=0)
 
 
-resps <- run_anderson_once(alpha=1, nlab=16, order="interspersed", encoding="encodeactual", bias=2)
-stims <- subset(resps, type=="STIM")
-clusts <- subset(resps, type=="CLUST")
-clustcounts <- daply( stims, .(clust), nrow )
+#resps <- run_anderson_once(alpha=1, nlab=16, order="interspersed", encoding="encodeactual", bias=2)
+#stims <- subset(resps, type=="STIM")
+#clusts <- subset(resps, type=="CLUST")
+#clustcounts <- daply( stims, .(clust), nrow )
 clustcounts['1']
 resps
 # }}}2
@@ -356,9 +351,9 @@ runs <- subset(runs, ! ((alpha==1 & order!="interspersed") | (alpha==1 & nlab==4
 nrow(runs)
 
 nreps <- 100
-#sims <- read.csv("bias.csv")
 ofile <- "hugegrid.csv"
-sims <- run_sims(runs, nreps, ofile)
+sims <- read.csv(ofile)
+#sims <- run_sims(runs, nreps, ofile)
 sims$nlab[sims$nlab==-1] <- Inf
 
 counts <- ddply(sims, .(nlab, alpha, sigma0, a0, lambda0, tau, order, bias_sd, encoding), function(x) summary(x$BestFit))
@@ -369,7 +364,16 @@ counts$twod <- counts$"2D"
 #print(ggplot(counts) + geom_line(aes(x=nlab, y=twod, group=params, linetype=factor(tau), colour=factor(alpha))) + facet_wrap(~order))
 
 # Subsetting
-semisup <- subset(counts, nlab==16 & Null<20 & bias_sd==0 & order=="interspersed")
+semisup <- subset(counts, nlab %in% c(0,16) & bias_sd==0 & order=="interspersed")
+countsquantified <- ddply(semisup, .(alpha, sigma0, a0, lambda0, tau, order, bias_sd, encoding), function(df) {
+      unlab <- subset(df, nlab==0)
+      lab <- subset(df, nlab==16)
+      denom <- function(x) x$twod + x$Unimodal + x$Bimodal #+ x$Null
+      unlabpercent <- unlab$Bimodal / denom(unlab)
+      labpercent <- lab$Bimodal / denom(lab)
+      data.frame(lab=labpercent, unlab=unlabpercent)})
+ggplot(countsquantified) + geom_point(aes(x=lab, y=unlab, colour=factor(sigma0))) + geom_abline() + facet_grid(lambda0~a0)
+head(semisup)
 arrange(semisup, twod)
 subset(counts, alpha==4 & sigma0==0.2 & lambda0==.5 & bias_sd==0 & a0==4)
 subset(counts, bias_sd==.5 & order=="interspersed" & alpha==1)
