6e46dda0c45ccd5eb4175526b7b6182ed2d56910
John McDonnell <john.v.mcdonnell@gmail.com>
2013-04-18 14:09:17 -0400

    Sims for publication.

--- fit_glc.R
+++ fit_glc.R
@@ -11,20 +11,19 @@ simulate_anderson(task="tvtask", alpha=2.333, nlab=-1, bias=0, a0=10, alab=1, la
 runs <- expand.grid(task="tvtask",
                     nlab=c(0,4,16,-1), 
                     order=c("interspersed", "labeledfirst", "labeledlast"),
-                    sigma0=c(.25),
-                    a0=c(1),
+                    sigma0=c(.125),
+                    a0=c(15),
                     alab=c(1),
                     lambda0=c(1),
                     alpha=c(1, .7/.3),
                     tau=c(.05), 
-                    bias_sd=c(0),
+                    bias_sd=c(0, 1.5),
                     encoding=c("encodeactual"))
 runs <- subset(runs, ! ((alpha==1 & order!="interspersed") | (alpha==1 & nlab==4)))
 nrow(runs)
 
-nreps <- 10
-ofile <- "search_sd.csv"
-ofile <- "andersonparams.csv"
+nreps <- 1000
+ofile <- "data/forpub.csv"
 sims <- read.csv(ofile)
 #sims <- run_sims(runs, nreps, ofile)
 sims$nlab[sims$nlab==-1] <- Inf
